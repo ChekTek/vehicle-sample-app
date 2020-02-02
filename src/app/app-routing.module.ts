5fbc81dc04fed867deba3454cbacb07b7d9466e5
ChekTek <zhoherchak@gmail.com>
2020-02-02 11:34:43 -0500

    initial commit, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'vehicle',
+    loadChildren: () => import('./vehicles/vehicles.module').then(m => m.VehiclesModule)
+  },
+  {
+    path: 'parts',
+    loadChildren: () => import('./parts/parts.module').then(m => m.PartsModule)
+  },
+  {
+    path: 'vehicles',
+    loadChildren: () => import('./vehicles/vehicles.module').then(m => m.VehiclesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
